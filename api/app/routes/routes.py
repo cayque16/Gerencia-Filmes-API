e24a480ebd522e524ffcd26c84e293862755f27c
Cayque Santos <cayque_santos16@hotmail.com>
2020-04-29 12:34:24 -0300

    Criada logica para retornar os filmes da base de dados.

--- api/app/routes/routes.py
+++ api/app/routes/routes.py
@@ -1,5 +1,11 @@
 from app import app
+from flask import jsonify
+from ..views import filmes
 
 @app.route('/', methods=['GET'])
 def root():
 	return "Teste"
+
+@app.route('/filmes', methods=['GET'])
+def get_filmes():
+	return filmes.get_filmes()
